3959381c79075571751b4652a5713273f7d98b06
amsel <vignesh.sc@zohocorp.com>
2024-08-23 00:20:15 +0530

    cross domain added

--- src/main/java/com/example/ToDOApp/controller/Fitcheck.java
+++ src/main/java/com/example/ToDOApp/controller/Fitcheck.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/fitcheck")
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "https://api.todoapp.online"})
 public class Fitcheck {
 
 
